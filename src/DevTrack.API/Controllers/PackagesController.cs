3d67e14eb38de7dab7a6e7d67a42ad662515834a
Tiago Candeias <tiago.g.candeias@gmail.com>
2022-05-15 00:53:02 +0100

    Solution updates and convert to filescopped namespaces

--- src/DevTrack.API/Controllers/PackagesController.cs
+++ src/DevTrack.API/Controllers/PackagesController.cs
@@ -1,63 +1,65 @@
 using System.Net;
 using DevTrack.Application.Features.Packages.DispatchPackage;
+using DevTrack.Application.Features.Packages.GetAllPackages;
 using DevTrack.Application.Features.Packages.UpdatePackageStatus;
 using DevTrack.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DevTrack.API.Controllers
+namespace DevTrack.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PackagesController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class PackagesController : ControllerBase
+    private readonly ILogger<PackagesController> _logger;
+    private readonly IMediator _mediator;
+
+    private readonly List<Package> _packages = new List<Package>{
+            new Package("Package 1", 1.3M),
+            new Package("Package 2", 1.1M)
+        };
+
+    public PackagesController(ILogger<PackagesController> logger, IMediator mediator)
     {
-        private readonly ILogger<PackagesController> _logger;
-        private readonly IMediator _mediator;
-
-        private readonly List<Package> _packages = new List<Package>{
-                new Package("Package 1", 1.3M),
-                new Package("Package 2", 1.1M)
-            };
-
-        public PackagesController(ILogger<PackagesController> logger, IMediator mediator)
-        {
-            _logger = logger;
-            _mediator = mediator;
-        }
+        _logger = logger;
+        _mediator = mediator;
+    }
 
 
-        [HttpGet]
-        public IActionResult GetAllPackages()
-        {
-            return Ok(_packages);
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAllPackagesAsync()
+    {
+        var request = new GetPackages.Request();
+        var packages = await _mediator.Send(request);
+        return Ok(packages);
+    }
 
 
-        [HttpGet("{code}")]
-        public IActionResult GetPackageByCode(Guid code)
-        {
-            return Ok(new Package("Package 1", 1.3M));
-        }
+    [HttpGet("{code}", Name = "GetByCode")]
+    public IActionResult GetPackageByCode(Guid code)
+    {
+        return Ok(new Package("Package 1", 1.3M));
+    }
 
-        [HttpPost]
-        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
-        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
-        [ProducesResponseType(typeof(DispatchPackage.Response), (int)HttpStatusCode.OK)]
-        public IActionResult Post(DispatchPackage.Request request)
-        {
-            _logger.LogInformation("Incoming!");
-            _mediator.Send(request);
-            return NoContent();
-        }
+    [HttpPost]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
+    // [ProducesResponseType(typeof(DispatchPackage.Response), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> PostAsync(DispatchPackage.Request request)
+    {
+        _logger.LogInformation("Incoming!");
+        var response = await _mediator.Send(request);
 
-        [HttpPost("{code}/update")]
-        public IActionResult Post(Guid code, UpdatePackageStatus.UpdateRequest request)
-        {
-            var package = new Package("Package 1", 2.5M);
-            package.StatusUpdate(PackageStatus.InTransit);
-            return NoContent();
-        }
+        return CreatedAtRoute("GetByCode", new { code = response.Package.Code }, response.Package);
+    }
 
+    [HttpPost("{code}/update")]
+    public IActionResult Post(Guid code, UpdatePackageStatus.UpdateRequest request)
+    {
+        var package = new Package("Package 1", 2.5M);
+        package.StatusUpdate(PackageStatus.InTransit);
+        return NoContent();
     }
 
 }
